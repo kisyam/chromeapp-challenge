aa2db5caf33084efed3152d0a1ecab19ad5ae2c8
kisyam <how2logout@gmail.com>
2023-01-20 23:10:43 +0900

    second challenge

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chrome App Challenge
+
+day 1
+day 2
---
+++ src/second.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+const body = document.querySelector("body");
+
+function changeBackground() {
+  const bodyWidth = window.innerWidth;
+  if (bodyWidth > 900) {
+    body.classList.remove("purple");
+    body.classList.add("yellow");
+  } else if (bodyWidth > 700) {
+    body.classList.remove("yellow");
+    body.classList.add("purple");
+  } else {
+    body.classList.remove("purple");
+  }
+}
+
+window.addEventListener("resize", changeBackground);
